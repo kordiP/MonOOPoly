f2021fab74ed7189e53ecc6696313aac298a956d
kordiP <nasikordov@gmail.com>
2025-06-24 23:27:18 +0300

    final

--- Sources/MonOOPoly.cpp
+++ Sources/MonOOPoly.cpp
@@ -6,14 +6,8 @@
 #include "../Headers/Utilities/HeterogenousContainer.hpp"
 #include "../Headers/Utilities/MyVector.hpp"
 #include "../Headers/Utilities/Stack.hpp"
-
-#include <cstdlib>
-#include <crtdbg.h>
-
 int main() 
 {
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-
 	MonopolyEngine& engine = MonopolyEngine::getInstance();
 	engine.run();
 }
